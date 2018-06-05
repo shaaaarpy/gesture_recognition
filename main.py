921d580ee91d681cf2d0ed4cd8e205f990d02910
sharpy013 <amit15013@iiitd.ac.in>
2018-06-05 10:52:37 +0530

    updating main file

--- main.py
+++ main.py
@@ -1,64 +1,57 @@
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from PIL import Image
-import scipy.misc
+import gesture_model
+import pickle
+import gzip
+from skimage import io
+from skimage.transform import resize
+import os
 
-train_labels=[]
-train_data=[]
-test_labels=[]
-test_data=[]
+def pickleModel(model, file_name):
+	pickle.dump(model, gzip.open(file_name, 'wb'))
 
 
-with open('./dataset/sign_mnist_train.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        train_labels.append(int(row['label']))
-        temp=[]
-        col='pixel'
-        for i in range(1,785):
-        	temp.append(int(row[col+str(i)]))
-        temp=np.array(temp)
-        train_data.append(temp)
-
-train_data=np.array(train_data)
-train_labels=np.array(train_labels)
-print(train_data.shape)
+def loadClassifier(file):
+	f = open(file, 'r+')
+	model = pickle.load(f)
+	f.close()
+	return model
 
 
-with open('./dataset/sign_mnist_test.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        test_labels.append(int(row['label']))
-        temp=[]
-        col='pixel'
-        for i in range(1,785):
-        	temp.append(int(row[col+str(i)]))
-        temp=np.array(temp)
-        test_data.append(temp)
+def pickleLoad(file_name):
+	return pickle.load(gzip.open(file_name, 'rb'))
 
-test_data=np.array(test_data)
-test_labels=np.array(test_labels)
-print(test_data.shape)
+direc='../dataset3/Dataset/'
 
-ind=0
-for i in train_data:
-    ind+=1
-    ir=i.reshape(28,28)
-    scipy.misc.toimage(ir, cmin=0, cmax=255).save('./image2/'+str(ind)+'.png')
-    # scipy.misc.toimage('./images/'+str(ind)+'.png', ir)
+gs = gesture_model.image_segment()
+userlist=['user_10','user_3','user_4','user_5','user_6','user_7','user_9']
+
+user_tr = userlist
+user_te = userlist[-1:]
 
 
-PCA = PCA(n_components=20)
-pca_train = PCA.fit(train_data)
-comp=pca_train.components_.T
-pca_train=np.dot(train_data, comp)
-pca_test=np.dot(test_data, comp)
-classif = RandomForestClassifier()
-classif.fit(pca_train, train_labels)
-print(classif.score(pca_test,test_labels))
+# gs.fit(user_tr)
 
+# pickleModel(gs, "sign_detector.pkl.gz")
+
+# print "The GestureRecognizer is saved to disk"
+
+# new_gr = pickleLoad("sign_detector.pkl.gz")
+handDetector=loadClassifier('./handDetector.pkl')
+new_gr=gesture_model.image_segment()
+print("Load Complete")
+
+directories=os.listdir(direc)
+for c in directories:
+	label=c
+	images=os.listdir(direc+c)
+	for im in images:
+		if im.split('.')[-1]=='jpg':
+			img=io.imread(direc+c+'/'+im)
+			filepath='./crop/'+c+'/'+im
+			new_gr.hand_segment(img, handDetector, filepath)
+		
+# img=io.imread('./Y2.jpg')
+# new_gr.hand_segment(img, handDetector, './cropped.png')
+# # new_gr=gs
+# img=resize(img, (160,120))
+# signDetector=loadClassifier('./signDetector.pkl')
+# label_encoder=loadClassifier('./label_encoder.pkl')
--- segment.py
+++ segment.py
@@ -9,32 +9,92 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from skimage.transform import resize
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
-def loadImages():
+
+# def loadImages():
+# 	image_array_ravel = []
+# 	label_array_ravel = []
+# 	base='test/'
+# 	directories=os.listdir(base)
+# 	directories.sort()
+# 	#print(directories)
+# 	counter=0
+# 	for folder in directories:
+# 		for filename in os.listdir(base+folder):
+# 			if 'depth' in filename:
+# 				img = cv.imread(base+folder+'/'+filename,0)
+# 				# img = cv.resize(img, dsize=(73, 128), interpolation=cv.INTER_CUBIC)
+# 				img=np.array(img)
+# 				image_array_ravel.append(img.ravel())
+# 				label_array_ravel.append(folder)
+# 	return np.array(image_array_ravel),np.array(label_array_ravel)
+
+
+def loadTest(base):
 	image_array_ravel = []
 	label_array_ravel = []
-	base='test/'
 	directories=os.listdir(base)
 	directories.sort()
-	#print(directories)
-	counter=0
 	for folder in directories:
 		for filename in os.listdir(base+folder):
-			if 'depth' in filename:
-				img = cv.imread(base+folder+'/'+filename,0)
-				# img = cv.resize(img, dsize=(73, 128), interpolation=cv.INTER_CUBIC)
+			if 'jpg' in filename:
+				img=cv.imread(base+folder+'/'+filename, 0)
+				img=resize(img, (73,128))
 				img=np.array(img)
 				image_array_ravel.append(img.ravel())
-				label_array_ravel.append(folder)
-	return np.array(image_array_ravel),np.array(label_array_ravel)
+				label_array_ravel.append(filename[0])
 
-image_array_ravel,label_array_ravel=loadImages()
-print(image_array_ravel.shape)
-print(label_array_ravel.shape)
+	return image_array_ravel, label_array_ravel
+
+image_array_ravel,label_array_ravel=loadTest('./submission/crop/')
+# print(image_array_ravel.shape)
+# print(label_array_ravel.shape)
 
 
 x_train, x_test, y_train, y_test=train_test_split(image_array_ravel,label_array_ravel, test_size=0.1, random_state=2)
 
+# print("Applying Without Transform")
+# clf=RandomForestClassifier()
+# clf.fit(x_train, y_train)
+# print(clf.score(x_test,y_test))
+
+
+# print("Applying PCA Transform")
+# PCA = PCA(n_components=20)
+# pca_train = PCA.fit(x_train)
+# comp=pca_train.components_.T
+# pca_train=np.dot(x_train, comp)
+# pca_test=np.dot(x_test, comp)
+# classif = RandomForestClassifier()
+# classif.fit(pca_train, y_train)
+# print(classif.score(pca_test,y_test))
+
+
+# print("Applying LDA Transformation")
+# lda=LDA().fit(x_train, y_train)
+# x_train_transform=lda.transform(x_train)
+# x_test_transform=lda.transform(x_test)
+# classif=RandomForestClassifier()
+# classif.fit(x_train_transform, y_train)
+# print(classif.score(x_test_transform, y_test))
+
+
+
+image_array_ravel,label_array_ravel=loadTest('./crop_skin/')
+
+x_train, x_test, y_train, y_test=train_test_split(image_array_ravel,label_array_ravel, test_size=0.1, random_state=2)
+
+print("Accuracy After Segmentation")
+
+print("Applying Without Transform")
+clf=RandomForestClassifier()
+clf.fit(x_train, y_train)
+print(clf.score(x_test,y_test))
+
+
+print("Applying PCA Transform")
 PCA = PCA(n_components=20)
 pca_train = PCA.fit(x_train)
 comp=pca_train.components_.T
@@ -44,7 +104,13 @@ classif = RandomForestClassifier()
 classif.fit(pca_train, y_train)
 print(classif.score(pca_test,y_test))
 
-
+print("Applying LDA Transformation")
+lda=LDA().fit(x_train, y_train)
+x_train_transform=lda.transform(x_train)
+x_test_transform=lda.transform(x_test)
+classif=RandomForestClassifier()
+classif.fit(x_train_transform, y_train)
+print(classif.score(x_test_transform, y_test))
 
 
 
